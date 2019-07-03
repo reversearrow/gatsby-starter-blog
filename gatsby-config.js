24e256a71f3e22ff3335dd812bbae81e0d840726
Jay Shah <reversearrow@gmail.com>
2019-07-03 17:31:49 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     {
     resolve: `gatsby-plugin-canonical-urls`,
     options: {
-        siteUrl: targetAddress.href.slice(0, -1),
+        siteUrl: `https://www.techsprite.ca`,
       },
     },
     {
